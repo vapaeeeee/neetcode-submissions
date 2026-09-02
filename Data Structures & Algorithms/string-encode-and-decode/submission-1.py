ba88171f34320356fdfa681806fdac756db06f43
vapaeeeee <75508384+vapaeeeee@users.noreply.github.com>
2026-09-02 15:11:39 -0300

    Add: string-encode-and-decode - submission-1

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-1.py
@@ -0,0 +1,28 @@
+class Solution:
+
+    #["Hello","World"]
+    def encode(self, strs: List[str]) -> str:
+        result = ""
+        for string in strs:
+            result += f"{len(string)}#{string}"
+        return result
+
+    #"5#Hello5#World11#like12#word"
+    def decode(self, s: str) -> List[str]:
+        pointer = 0
+        result: List[str] = [] 
+        while pointer < len(s):
+            reachedDilimiter = False
+            length = ""
+            while reachedDilimiter is False:
+                if s[pointer] == "#":
+                    reachedDilimiter = True
+                    pointer += 1
+                    break
+                length += s[pointer]
+                pointer += 1
+            final_pos = pointer + int(length)
+            string = s[pointer:final_pos]
+            result.append(string)
+            pointer = final_pos
+        return result
